620e9856dc84bac6819302059a241f3194f0828b
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2025-06-19 08:25:45 +0200

    Rename main script.

--- wifite.py
+++ wifite.py
@@ -3,5 +3,5 @@
 # Note: This script runs Wifite from within a cloned git repo.
 # The script `bin/wifite` is designed to be run after installing (from /usr/sbin), not from the cwd.
 
-from wifite import __main__
-__main__.entry_point()
+from wifite import wifite
+wifite.main()
